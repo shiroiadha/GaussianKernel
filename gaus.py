7d02cc4a11e5af75501428ce74d39b057095872d
shiroiadha <adharasyid2005@gmail.com>
2025-04-06 12:53:57 +0700

    Fist Upload (Rev 1)

--- gaus.py
+++ gaus.py
@@ -2,7 +2,6 @@ import cv2, math
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Fungsi untuk menerapkan Gaussian Blur dengan kernel custom
 def apply_gaussian_blur(image_path, kernel_size=5, sigma=1.0):
     # Baca gambar
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
@@ -11,7 +10,6 @@ def apply_gaussian_blur(image_path, kernel_size=5, sigma=1.0):
         print("Gagal memuat gambar. Periksa path-nya.")
         return
 
-    # Buat Gaussian kernel secara manual
     def gaussian_kernel(size, sigma):
         ax = np.linspace(-(size - 1) / 2., (size - 1) / 2., size)
         xx, yy = np.meshgrid(ax, ax)
@@ -20,10 +18,8 @@ def apply_gaussian_blur(image_path, kernel_size=5, sigma=1.0):
 
     kernel = gaussian_kernel(kernel_size, sigma)
 
-    # Terapkan filter Gaussian menggunakan kernel custom
     blurred = cv2.filter2D(image, -1, kernel)
 
-    # Tampilkan hasil
     plt.figure(figsize=(10, 5))
     plt.subplot(1, 2, 1)
     plt.title("Original")
@@ -37,7 +33,6 @@ def apply_gaussian_blur(image_path, kernel_size=5, sigma=1.0):
     
     plt.show()
 
-# Ganti dengan path ke file gambarmu
 sigma = 1
 kernel_size = math.ceil(6* sigma)
 apply_gaussian_blur('blendNgeGabut.png', kernel_size, sigma)
